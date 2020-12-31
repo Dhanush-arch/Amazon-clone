0c4a63c027d68718f681d1d69afb200a29426f74
Dhanush <dhanush18245@gmail.com>
2020-12-31 18:49:25 +0530

    Added API wrapper for Django BackEnd API

---
+++ src/apiWrapper.js
@@ -0,0 +1,132 @@
+import axios from "axios";
+
+const base_url = "http://localhost:8000/api/v1";
+
+let _token;
+
+export const login = (user_name, password) => {
+  return axios
+    .post(base_url + `/rest-auth/login/`, {
+      username: user_name,
+      email: user_name,
+      password: password,
+    })
+    .then((e) => {
+      _token = e.data.key;
+      return e.data.key;
+    });
+};
+
+export const logout = () => {
+  /** returns 200 if logout is successful */
+
+  return axios
+    .post(base_url + `/rest-auth/logout/`, {
+      headers: {
+        Authorization: `token ${_token}`,
+      },
+    })
+    .then((e) => {
+      console.log(e.status);
+    });
+};
+
+export const register = (user_name, password1, password2) => {
+  /** returns 201 and token key if registeration is successful */
+
+  return axios
+    .post(base_url + `/rest-auth/registration/`, {
+      username: user_name,
+      email: user_name,
+      password1: password1,
+      password2: password2,
+    })
+    .then((e) => {
+      _token = e.data.key;
+      return { status: e.status, key: e.data.key };
+    });
+};
+
+export const getProducts = (id) => {
+  /** Returns all the products if id=0 and
+   *  Returns the specific product based on the product ID */
+  return axios.get(base_url + `/products/${id}/`).then((e) => {
+    return e.data;
+  });
+};
+
+export const getCart = (id) => {
+  /** Returns the specific Cart based on the User ID */
+  if (_token) {
+    return axios
+      .get(base_url + `/cart/${id}/`, {
+        headers: {
+          Authorization: `token ${_token}`,
+        },
+      })
+      .then((e) => {
+        return e.data;
+      });
+  } else {
+    return 0;
+  }
+};
+
+export const getCategory = () => {
+  return axios.get(base_url + `/category/`).then((e) => {
+    return e.data;
+  });
+};
+
+export const addToCart = (user_id, product_id) => {
+  /** Return String "yes" if add to cart is successfull */
+  if (_token) {
+    return axios
+      .post(
+        base_url + "/add-to-cart/",
+        { user: user_id, productId: product_id },
+        {
+          headers: {
+            Authorization: `token ${_token}`,
+          },
+        }
+      )
+      .then((e) => {
+        return e.data;
+      });
+  }
+};
+
+export const deleteFromCart = (user_id, product_id, cart_id) => {
+  if (_token) {
+    return axios
+      .delete(
+        base_url + `/delete-from-cart/${product_id}/${cart_id}/${user_id}/`,
+        {
+          headers: {
+            Authorization: `token ${_token}`,
+          },
+        }
+      )
+      .then((e) => {
+        return e;
+      });
+  }
+};
+
+export const decreaseFromCart = (user_id, products_id, cart_id) => {
+  if (_token) {
+    return axios
+      .put(
+        base_url + `/decrease-from-cart/${products_id}/${cart_id}/${user_id}/`,
+        
+         { headers: {
+            Authorization: `token ${_token}`,
+          },}
+        
+      )
+      .then((e) => {
+        return e;
+      });
+  }
+};
